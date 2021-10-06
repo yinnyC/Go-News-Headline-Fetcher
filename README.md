8e3a22efd4abbe269bd9da16b62b49a5803b7434
Yin <yinnnn17@gmail.com>
2021-10-06 16:10:27 -0700

    [update] finished asyncHTTP func

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # MakeUtility
+
+[![Go Report Card](https://goreportcard.com/badge/github.com/yinnyC/MakeUtility)](https://goreportcard.com/report/github.com/yinnyC/MakeUtility)
---
+++ main.go
@@ -0,0 +1,134 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"sync"
+	"time"
+
+	"github.com/joho/godotenv"
+)
+
+// number of parallelism
+const NUM_PARALLEL = 5
+
+// A Response struct to map the Entire Response
+type Response struct {
+	Category string    `json:"category"`
+	Articles []Article `json:"articles"`
+}
+
+type Article struct {
+	Title string `json:"title"`
+	Url   string `json:"url"`
+}
+type result struct {
+	News Response
+	err  error
+}
+
+func makeUrl(category string) string {
+	// This function return an URL for topnewsheadlines request.
+	godotenv.Load(".env")
+	API_KEY := os.Getenv("API_KEY")
+	if category != "" {
+		return fmt.Sprintf("https://newsapi.org/v2/top-headlines?country=us&category=%s&apiKey=%s", category, API_KEY)
+	}
+	return fmt.Sprintf("https://newsapi.org/v2/top-headlines?country=us&apiKey=%s", API_KEY)
+}
+
+func fetchNewsHeadline(category string) (Response, error) {
+	url := makeUrl(category)
+	response, err := http.Get(url)
+	if err != nil {
+		fmt.Print(err.Error())
+		os.Exit(1)
+	}
+	responseData, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	var responseObject Response
+	json.Unmarshal(responseData, &responseObject)
+	responseObject.Category = category
+	return responseObject, err
+}
+
+// Stream news to input channel
+func streamNews(done <-chan struct{}, inputs []string) <-chan string {
+	inputCh := make(chan string)
+	go func() {
+		defer close(inputCh)
+		for _, input := range inputs {
+			select {
+			case inputCh <- input:
+			case <-done:
+				// in case done is closed prematurely (because error midway),
+				// finish the loop (closing input channel)
+				break
+			}
+		}
+	}()
+	return inputCh
+}
+
+func AsyncHTTP(categories []string) ([]Response, error) {
+	done := make(chan struct{})
+	defer close(done)
+
+	inputCh := streamNews(done, categories)
+
+	var wg sync.WaitGroup
+	// bulk add goroutine counter at the start
+	wg.Add(NUM_PARALLEL)
+
+	resultCh := make(chan result)
+
+	for i := 0; i < NUM_PARALLEL; i++ {
+		// spawn N worker goroutines, each is consuming a shared input channel.
+		go func() {
+			for input := range inputCh {
+				response, err := fetchNewsHeadline(input)
+				resultCh <- result{response, err}
+			}
+			wg.Done()
+		}()
+	}
+
+	// Wait all worker goroutines to finish. Happens if there's no error (no early return)
+	go func() {
+		wg.Wait()
+		close(resultCh)
+	}()
+
+	results := []Response{}
+	for result := range resultCh {
+		if result.err != nil {
+			// return early. done channel is closed, thus input channel is also closed.
+			// all worker goroutines stop working (because input channel is closed)
+			return nil, result.err
+		}
+		results = append(results, result.News)
+	}
+
+	return results, nil
+}
+
+func main() {
+	categories := []string{"business", "general", "science", "technology", "health"}
+	start := time.Now()
+
+	results, err := AsyncHTTP(categories)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	for _, result := range results {
+		fmt.Println(result)
+	}
+	fmt.Println("finished in ", time.Since(start))
+}
